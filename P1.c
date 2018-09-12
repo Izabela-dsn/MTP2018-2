5ee0e6f1626388bf263ce68477a54bfde59cb1f1
IzabelaUFU <izabeladasilva10@gmail.com>
2018-09-11 21:53:08 -0300

    P1.c

---
+++ P1.c
@@ -0,0 +1,43 @@
+/* P1.c */
+/* Izabela da Silva */
+/* 11811ECP026 */ 
+
+#include <stdio.h>
+
+int main(){
+	int estado = 0, i; 
+	char binario[256];
+	
+	
+	scanf("%s", binario);
+	
+	while(binario[i] != '\0'){
+		if(estado == 0 && binario[i] == '0'){
+			estado = 0;
+		}
+		else if(estado == 0 && binario[i] == '1'){
+			estado = 1;
+		}
+		else if(estado == 1 && binario[i] == '0'){
+			estado = 2;
+		}
+		else if(estado == 1 && binario[i] == '1'){
+			estado = 0;
+		}
+		if(estado == 2 && binario[i] == '1'){
+			estado = 2;
+		}
+		else if(estado == 2 && binario[i] == '0'){
+			estado = 1;
+		}
+		if(estado == 0){
+		printf("\"%s\" e multiplo de 3\n", binario, estado);
+		}
+	    else if(estado == 1){
+		printf("\"%s\" nao e\n", binario, estado);
+		} 
+		break;
+	}
+	
+	return 0;
+}
